f37f18877220f522ee95326bff50410a511fc1d7
Ubuntu <ubuntu@ip-172-31-20-243.ec2.internal>
2019-09-23 23:08:46 +0000

    message

---
+++ pizza.cpp
@@ -0,0 +1,34 @@
+// Copyright (c) 2019 Christina Ngwa All rights reserved.
+//
+// Created by: Christina Ngwa
+// Created on: September 2019
+// This program calculates the cost of pizza
+
+#include <iostream>
+#include <iomanip>
+
+int main() {
+    // this function calculates the cost of pizza
+    const float LABOR = 0.75;
+    const float RENT = 1.00;
+    const float COST_PER_INCH = 0.50;
+    const float HST = 0.13;
+    float diameter;
+    float sub_total;
+    float total;
+
+    // input
+    std::cout << "Enter the diameter of the pizza you would like (inch): ";
+    std::cin >> diameter;
+
+    // process
+    sub_total = LABOR + RENT + (diameter * COST_PER_INCH);
+    total = sub_total + (sub_total * HST);
+
+    // output
+    std::cout << "" << std::endl;
+    std::cout << "The cost for a " << diameter << " inch pizza is: $"
+
+              << std::fixed << std::setprecision(2) << std::setfill('0')
+              << total << "." << std::endl;
+}
